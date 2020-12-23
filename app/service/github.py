23bf2bdddc632c855aebeb9b552ebb96f93057e8
lucas-rds <lucas.rodrigues6673@gmail.com>
2020-12-23 12:08:15 -0300

    Override comment on github

--- app/service/github.py
+++ app/service/github.py
@@ -8,11 +8,13 @@ from app.domain.services.base_github import BaseGithub
 
 class Github(BaseGithub):
     def fetch_repositories_by_username(self, username: str) -> json:
+        """Overrides BaseGithub.fetch_repositories_by_username()"""
         response = requests.get(f"{GITHUB_URL}/users/{username}/repos")
         response.raise_for_status()
         return response.json()
 
     def fetch_repository_by_user_and_name(self, username: str, repository_name: str) -> json:
+        """Overrides BaseGithub.fetch_repository_by_user_and_name()"""
         response = requests.get(
             f"{GITHUB_URL}/repos/{username}/{repository_name}")
         response.raise_for_status()
